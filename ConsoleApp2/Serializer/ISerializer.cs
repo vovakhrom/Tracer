50e9be70a093c286696be758803373293a9a5cb4
Vladimir <lililolo1222@mail.ru>
2021-10-26 21:25:41 +0300

    Add interface ISerializer

---
+++ ConsoleApp2/Serializer/ISerializer.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Listsoft
+{
+    namespace Lab_Tracer
+    {
+        namespace Serilization
+        {
+            public interface ISerializer
+            {
+                string Serialize(TraceResult traceResult);
+                string GetExtension();
+            }
+        }
+    }
+}
