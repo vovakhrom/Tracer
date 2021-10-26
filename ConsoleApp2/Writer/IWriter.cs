ffe515080089d053459960c68f43c6e982b208fd
Vladimir <lililolo1222@mail.ru>
2021-10-26 21:28:06 +0300

    Add Interface IWriter

---
+++ ConsoleApp2/Writer/IWriter.cs
@@ -0,0 +1,31 @@
+﻿using Listsoft.Lab_Tracer.Serilization;
+
+namespace Listsoft
+{
+    namespace Lab_Tracer
+    {
+        namespace Writer
+        {
+            public interface IWriter
+            {
+                void Write(ISerializer serializer, TraceResult traceResult);
+            }
+            public class WriterBridge
+            {
+                private IWriter _currentInterface = null;
+                public WriterBridge()
+                {                    
+                }
+                public WriterBridge(IWriter writer) {
+                    _currentInterface = writer;
+                }
+                public void SetIntarface(IWriter writer) {
+                    _currentInterface = writer;
+                }
+                public void Write(ISerializer serializer, TraceResult traceResult) {
+                    _currentInterface.Write(serializer, traceResult);
+                }
+            }
+        }
+    }
+}
