83e38c52e2b1aa6941e1977cff1f4db511b6da50
Vladimir <lililolo1222@mail.ru>
2021-10-26 21:26:14 +0300

    Add JsonSerializer

---
+++ ConsoleApp2/Serializer/JsonSerializer.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Newtonsoft.Json;
+
+
+namespace Listsoft
+{
+    namespace Lab_Tracer
+    {
+        namespace Serilization
+        {
+            public class JsonSerializer : ISerializer
+            {
+                public string Serialize(TraceResult traceResult)
+                {
+                    return JsonConvert.SerializeObject(traceResult, Formatting.Indented);
+                }
+                public string GetExtension()
+                {
+                    return ".json";
+                }
+            }
+        }
+    }
+}
