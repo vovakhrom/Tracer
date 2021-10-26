a253a789dcaf96640652acee34f5b1e401da0955
Vladimir <lililolo1222@mail.ru>
2021-10-26 21:28:35 +0300

    Add ConsoleWriter

---
+++ ConsoleApp2/Writer/ConsoleWriter.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Listsoft.Lab_Tracer.Serilization;
+
+namespace Listsoft
+{
+    namespace Lab_Tracer
+    {
+        namespace Writer
+        {
+            public class ConsoleWriter : IWriter
+            {
+                public void Write(ISerializer serializer, TraceResult traceResult)
+                {
+                    Console.WriteLine(serializer.Serialize(traceResult));
+                }
+            }
+        }
+    }
+}
