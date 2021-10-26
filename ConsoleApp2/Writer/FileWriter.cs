4f5f55546b178d17a4c515fa9dd17efb8b0dcb53
Vladimir <lililolo1222@mail.ru>
2021-10-26 21:29:04 +0300

    Add FileWriter

---
+++ ConsoleApp2/Writer/FileWriter.cs
@@ -0,0 +1,22 @@
+﻿using System.IO;
+using Listsoft.Lab_Tracer.Serilization;
+
+namespace Listsoft
+{
+    namespace Lab_Tracer
+    {
+        namespace Writer
+        {
+            public class FileWriter : IWriter
+            {
+                public void Write(ISerializer serializer, TraceResult traceResult)
+                {
+                    using (StreamWriter sw = new StreamWriter("traceResult" + serializer.GetExtension()))
+                    {
+                        sw.WriteLine(serializer.Serialize(traceResult));
+                    }
+                }
+            }
+        }
+    }
+}
